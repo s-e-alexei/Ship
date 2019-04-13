ee1f3e2bf6d405d521e1b17c18c359d485a0305c
acdc@plaz.com.ua <acdc@plaz.com.ua>
2019-04-14 02:14:01 +0300

    publish

---
+++ Ship/Cargo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Ship
+{
+
+    class Cargo
+    {
+        private Double level;
+        private Double volume;
+        private Double x;
+        private Double y;
+        private Double z;
+        private Double square;
+        private Double freeX;
+        private Double freeY;
+        private Double freeIx;
+        private Double freeIy;
+
+        public double Level { get => level; set => level = value; }
+        public double Volume { get => volume; set => volume = value; }
+        public double X { get => x; set => x = value; }
+        public double Y { get => y; set => y = value; }
+        public double Z { get => z; set => z = value; }
+        public double Square { get => square; set => square = value; }
+        public double FreeX { get => freeX; set => freeX = value; }
+        public double FreeY { get => freeY; set => freeY = value; }
+        public double FreeIx { get => freeIx; set => freeIx = value; }
+        public double FreeIy { get => freeIy; set => freeIy = value; }
+
+        public Cargo (Double level, Double volume, Double x, Double y, Double z, Double square, Double freeX, Double freeY, Double freeIx, Double freeIy)
+        {
+            this.level = level;
+            this.volume = volume;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.square = square;
+            this.freeX  = freeX;
+            this.freeY =    freeY;
+            this.freeIx = freeIx;
+            this.FreeIy = freeIy;
+
+
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Level},{Volume},{X}, {Y}, {Z} {Square}, {FreeX},{FreeY}, {FreeIx},{FreeIy} , m\n";
+        }
+
+
+
+
+
+
+
+
+        //class Cargo
+        //{
+        //    public void Main(string[] args)
+        //        {            
+        //        double[, ,] Cargo1 = new double[0, 0, 0];
+        //        double[, ,] Cargo2 = new double[0, 0, 0];
+        //        double[, ,] Cargo3 = new double[0, 0, 0];
+        //        double Weight, StowageFactor, Volume;
+
+        //        Cargo1 = new Cargo1 [Weight, StowageFactor, Volume];
+        //        for (int w = 0; w < Weight.Length; w++)
+        //        {
+        //            for (int s = 0; s < StowageFactor.Length; s++)
+        //            {
+        //                for (int v = 0; v < Volume.length; v++)
+        //                {
+        //                    Cargo1[w,s,v] = ref.Next()
+        //                }
+        //            }
+        //        }
+        //        Cargo2 = new double[Weight, StowageFactor, Volume];
+        //        Cargo3 = new double[Weight, StowageFactor, Volume];
+
+        //        const double Weight = 6;
+        //        const double StowegFactor = 12;
+        //        const double firms = 6;
+
+        //    }
+    }
+}
---
+++ Ship/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ship
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Double []> st = new List<Double []>();
+
+            try
+            {
+                foreach (string line in System.IO.File.ReadLines(@"C:\\Users\\USER\\source\\repos\\Ship\\Ship\\MarineRepository\\Compartments\\Data\\CargoHold1.csv"))
+                    st.Add(line.Trim().Replace('.',',').Split(';').Select(double.Parse).ToArray());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("NOT READ");
+                Console.WriteLine(e.Message);
+            }
+            List<Cargo> cargo = new List<Cargo>();
+            foreach (Double[] item in st)
+            {
+                cargo.Add(new Cargo(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9]));
+                foreach (var subitem in item)
+                {
+                    Console.Write(subitem + " ");
+                }
+                Console.WriteLine();
+            }
+            //Console.WriteLine(st[5][9]);
+            //Console.Read();
+
+            Console.WriteLine("Input x:");
+            double x = 150;//Convert.ToDouble(Console.ReadLine());
+            int x1 = -1, x2 = -1;
+            for (int i = 0; i < cargo.Count; i++)
+            {
+                //Console.WriteLine(st1[0]);
+                    
+                if (cargo[i].Volume > x)
+                {
+                    x1 = i - 1;
+                    x2 = i;
+                    break;
+                }
+            
+            }
+            Console.WriteLine(x1 + "   " + x2);
+            double y = ((cargo[x1].Y == cargo[x2].Y) ? cargo[x1].Y : Math.Abs((x - x1) * (cargo[x1].Y - cargo[x2].Y) / (x2 - x1)));
+             Console.WriteLine(y);
+
+
+            Console.Read();
+
+        }
+
+
+        //double[,] v = new double[5, 2];
+        //v[0, 0] = 0.1; v[0, 1] = 64.69;
+        //v[1, 0] = 1.1; v[1, 1] = 1673;
+        //v[2, 0] = 2.1; v[2, 1] = 3468;
+        //v[3, 0] = 3.1; v[3, 1] = 5311;
+        //v[4, 0] = 4.1; v[4, 1] = 7177;
+
+
+        //delegate void UI
+
+
+        //class MyEvent
+        //{
+        //    // Объявляем событие
+        //    public event UI UserEvent;
+
+        //    // Используем метод для запуска события
+        //    public void OnUserEvent()
+        //    {
+        //        UserEvent();
+        //    }
+        //}
+
+        //class UserInfo
+        //{
+        //    string uiName, uiFamily;
+        //    int uiAge;
+
+        //    public UserInfo(string Name, string Family, int Age)
+        //    {
+        //        this.Name = Name;
+        //        this.Family = Family;
+        //        this.Age = Age;
+        //    }
+
+
+
+
+    }
+}
+
+
